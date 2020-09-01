aa0f7a153f37dbcefde575a0d7920d7dadb1de82
ben-jamming-reilly <benreilly0809@gmail.com>
2020-09-01 15:11:09 -0700

    new file:   client/.gitignore 	new file:   client/README.md 	new file:   client/package-lock.json 	new file:   client/package.json 	new file:   client/public/favicon.ico 	new file:   client/public/index.html 	new file:   client/public/logo192.png 	new file:   client/public/logo512.png 	new file:   client/public/manifest.json 	new file:   client/public/robots.txt 	new file:   client/src/App.css 	new file:   client/src/App.js 	new file:   client/src/App.test.js 	new file:   client/src/index.css 	new file:   client/src/index.js 	new file:   client/src/logo.svg 	new file:   client/src/serviceWorker.js 	new file:   client/src/setupTests.js 	modified:   package.json 	modified:   server.js

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const path = require("path");
-const body = require("body-parser");
+const bodyParser = require("body-parser");
 const connectDB = require("./config/db");
 
 connectDB();
 
 const app = express();
+
+// To parse application/json
+app.use(bodyParser.json());
+
+// Routes Go Here
+// --Orders (A Reciept Essentially)
+// --Items (The food that will be sold)
+// --Users (The Business End)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server started on " + PORT));
